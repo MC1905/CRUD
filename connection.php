3744a3bfa809fdabe3711f6a053ccfd683ecab34
MC1905 <merdancan1702@gmail.com>
2022-04-22 08:31:04 +0200

    1

---
+++ connection.php
@@ -0,0 +1,23 @@
+<?php
+  
+$host = 'localhost';
+   
+try {
+    $servername = "localhost:3306";
+    $dbname = "loginPage";
+    $username = "root";
+    $password = "";
+   
+    $connect = new PDO(
+        "mysql:host=$servername; dbname=loginPage",
+        $username, $password
+    );
+      
+   $connect->setAttribute(PDO::ATTR_ERRMODE,
+                    PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+  
+?>
